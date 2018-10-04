40d8a0b5fe5a45abbf401a92e621a16b61d6c348
jackybethea1234 <jackybethea@gmail.com>
2018-10-04 19:49:40 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,12 +2,13 @@
 
 
 def least_coins(cents)
+  
    numquarters = cents / 25
    numdimes = (cents-numquarters*25) / 10
    numnickels = (cents -(numquarters*25+ numdimes*10)) / 5
    numpennies = cents - ((numquarters*25) + (numdimes*10) + (numnickels*5))
 coins ={:quarters => numquarters, :dimes => numdimes, :nickels => numnickels, :pennies => numpennies}
-#Code your answer here!
+
 end
 
 least_coins(29)
---
+++ state_capitol.rb
@@ -0,0 +1,10 @@
+states = {"massachusetts"=> "MA", "wisconsin"=> "WI", "new jersey"=> "NJ", "colorado"=> "CO"}
+
+#states.each do |state,code|
+ # puts state
+#end
+
+states.each_key {|state| puts state}
+
+
+
